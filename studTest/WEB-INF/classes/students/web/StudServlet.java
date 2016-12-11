ae61b7c550810b776860b54a925aaa5ccc04499a
SkiTLi <kotinsandy@gmail.com>
2016-12-12 00:09:42 +0300

    Add files via upload

--- studTest/WEB-INF/classes/students/web/StudServlet.java
+++ studTest/WEB-INF/classes/students/web/StudServlet.java
@@ -20,101 +20,115 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class StudServlet extends javax.servlet.http.HttpServlet {    
-    protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-	String motion=request.getParameter("mot");	//переменной присваивается параметр действия
-	if (motion == null){
-	    printListStud(request, response);// именно в таком порядке process2 потом 1
-	    //printParam(request, response);	//печатает все параметры страницы
-	}else{ 
-	    switch (motion){
-		case "update":  
-		    request.getRequestDispatcher("/WEB-INF/otherjsp/updateStud.jsp").forward(request, response);//переход на другую jsp
-		    break;
-		case "delete":  
-		    request.getRequestDispatcher("/WEB-INF/otherjsp/deleteStud.jsp").forward(request, response);//переход на другую jsp
-		    break;
-		case "add":  
-		    request.getRequestDispatcher("/WEB-INF/otherjsp/addStud.jsp").forward(request, response);//переход на другую jsp
-		    break;
-	    }
+public class StudServlet extends javax.servlet.http.HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String motion  = request.getParameter("mot");    //РїР°СЂР°РјРµС‚СЂ РѕРїСЂРµРґРµР»СЏСЋС‰РёР№ РґРµР№СЃС‚РІРёРµ
+	/*	if (motion == null) {
+			// РёРјРµРЅРЅРѕ РІ С‚Р°РєРѕРј РїРѕСЂСЏРґРєРµ mAct() Р·Р°С‚РµРј printListStud() Р·Р°С‚РµРј printParam()
+			mAct(request, response);// РѕР±РЅРѕРІР»СЏРµС‚, СѓРґР°Р»СЏРµС‚, РґРѕР±Р°РІР»СЏРµС‚ СЃС‚СѓРґРµРЅС‚Р°
+			printListStud(request, response);// РїРµС‡Р°С‚РµС‚ СЃС‚СѓРґРµРЅС‚РѕРІ Рё РєРЅРѕРїРєРё СЂРµРґР°РєС‚РёСЂРѕРІР°РЅРёСЏ/СѓРґР°Р»РµРЅРёСЏ
+			//printParam(request, response);	//РїРµС‡Р°С‚Р°РµС‚ РІСЃРµ РїР°СЂР°РјРµС‚СЂС‹
+		} else {
+	*/
+			switch (motion) {
+				case "update":
+					request.getRequestDispatcher("/WEB-INF/otherjsp/updateStud.jsp").forward(request, response);//РїРµСЂРµС…РѕРґРёС‚ РЅР° РґСЂСѓРіСѓСЋ jsp
+					break;
+				case "delete":
+					request.getRequestDispatcher("/WEB-INF/otherjsp/deleteStud.jsp").forward(request, response);//РїРµСЂРµС…РѕРґРёС‚ РЅР° РґСЂСѓРіСѓСЋ jsp
+					break;
+				case "add":
+					request.getRequestDispatcher("/WEB-INF/otherjsp/addStud.jsp").forward(request, response);//РїРµСЂРµС…РѕРґРёС‚ РЅР° РґСЂСѓРіСѓСЋ jsp
+					break;
+				// РёРјРµРЅРЅРѕ РІ С‚Р°РєРѕРј РїРѕСЂСЏРґРєРµ mAct() Р·Р°С‚РµРј printListStud() Р·Р°С‚РµРј printParam(), РїРѕСЌС‚РѕРјСѓ Р±РµР· break
+				case "action":
+					mAct(request, response);// РѕР±РЅРѕРІР»СЏРµС‚, СѓРґР°Р»СЏРµС‚, РґРѕР±Р°РІР»СЏРµС‚ СЃС‚СѓРґРµРЅС‚Р°
+				case "print":
+					printListStud(request, response);// РїРµС‡Р°С‚РµС‚ СЃС‚СѓРґРµРЅС‚РѕРІ Рё РєРЅРѕРїРєРё СЂРµРґР°РєС‚РёСЂРѕРІР°РЅРёСЏ/СѓРґР°Р»РµРЅРёСЏ
+					break;
+
+
+			}
+	//	}
 	}
-    }
 
     private void printListStud(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException {
-        resp.setContentType("text/html;charset=utf-8"); //utf-8//Windows-1251//было "text/html;charset=Windows-1251"
-	PrintWriter pw = resp.getWriter();
-        pw.println("<B>если убрать *;charset=Windows-1251||utf-8*, то русского текста не видно</B>");
-        pw.println("<table border=3>");	
-	//pw.println(resp);//ничего интересного не показывает
+        resp.setContentType("text/html;charset=utf-8"); //utf-8//Windows-1251//???? "text/html;charset=Windows-1251"
+		PrintWriter pw = resp.getWriter();
+        pw.println("<B>РµСЃР»Рё СѓР±СЂР°С‚СЊ *;charset=Windows-1251||utf-8*, С‚Рѕ СЂСѓСЃСЃРєРѕРіРѕ С‚РµРєСЃС‚Р° РЅРµ РІРёРґРЅРѕ</B>");
+        pw.println("<table border=3>");
+		try {
+			List<Student> l = StudentDao.getInstance().getAllStudents();
+			for (Student s:l){
+				pw.println("<tr>");
+				pw.println("<td>" + s.getId() + "</td>");
+				pw.println("<td>" + s.getName() + "</td>");
+				pw.println("<td>" + s.getBirthday() + "</td>");
+				pw.println("<form action = \"servStud\" method=\"GET\"><td colspan = \"19\">"
+					+ "<input type=\"hidden\" name=\"id\" value=\"" + s.getId()
+					+ "\" />	<input type=\"hidden\" name=\"name\" value=\"" + s.getName()
+					+ "\" />	<input type=\"hidden\" name=\"birthday\" value=\"" + s.getBirthday()
+					+ "\" />	<input type=\"hidden\" name=\"mot\" value=\"update\" />"	//??????????? ??????????? ????????, ??????? ?????????? ???????? ? ???? ?? ????????
+					+"<input type = \"submit\" value = \"Р РµРґ(Update)\"/>	</td></form>");
+				pw.println("<form action = \"servStud\" method=\"GET\"><td colspan = \"19\">"
+					+ "<input type=\"hidden\" name=\"id\" value=\""	+ s.getId()
+					+ "\" />	<input type=\"hidden\" name=\"name\" value=\"" + s.getName()
+					+ "\" />	<input type=\"hidden\" name=\"birthday\" value=\"" + s.getBirthday()
+					+ "\" />	<input type=\"hidden\" name=\"mot\" value=\"delete\" />" //??????????? ??????????? ????????, ??????? ?????????? ???????? ? ???? ?? ????????
+					+"<input type = \"submit\" value = \"РЈРґР°Р»(Delete)\"/>	</td></form>");
+				pw.println("</tr>");
+			}
+			pw.println("<form action = \"servStud\" method=\"GET\"><td colspan = \"19\">"
+				+ "<input type=\"hidden\" name=\"mot\" value=\"add\" />"				//??????????? ??????????? ????????, ??????? ?????????? ???????? ? ???? ?? ????????
+				+ "<input type = \"submit\" value = \"РќРѕРІ. СЃС‚СѓРґ (Add a new student)\"/>	</td></form>");
+		} catch (Exception e) {
+			throw new ServletException("EРіРі0R. you make some mistake!",e);
+		}
+			pw.println("</table>");
+	}
 
-	try {
-//---***эта часть кода должна идти перед выводом таблицы студентов (чтобы в нее попали обновленные значения):
-	    String act="";
-	    if (req.getParameter("act3")!=null){
-	        act=req.getParameter("act3");
-	    }
-	    int id1=888;
-	    if (req.getParameter("id")!=null){
-	        id1=Integer.parseInt(req.getParameter("id"));//parse нужен для преобразования string в int
-	    }
-	    String name1="empty8";
-	    if (req.getParameter("name")!=null){
-	        name1=req.getParameter("name");	
-	    }    
-	    Student stud = new Student();
-	    stud.setId(id1);
-	    stud.setName(name1);
-	    switch (act){
-		case "add":		
-		StudentDao.getInstance().addStudent(stud);
-		//pw.println("должен добавиться студентик");
-		break;
-		case "update":		
-		StudentDao.getInstance().updateStudent(stud);
-		//pw.println("должен обновиться студентик");
-		break;
-		case "delete":				
-		StudentDao.getInstance().deleteStudent(stud);
-		//pw.println("должен удалиться студентик");
-		break;
-		default:
-		break;
-	    }
-//---***см. выше
-	    //StudentDao.getInstance().getAllStudents();	    
-            List<Student> l = StudentDao.getInstance().getAllStudents();	    
-	    for (Student s:l){           
-                pw.println("<tr>");				
-                pw.println("<td>" + s.getId() + "</td>"); 
-		pw.println("<td>" + s.getName() + "</td>");
-		pw.println("<td>" + s.getBirthday() + "</td>");		
-		pw.println("<form action = \"servStud\" method=\"GET\"><td colspan = \"19\"><input type=\"hidden\" name=\"id\" value=\""
-			+ s.getId() + "\" />	<input type=\"hidden\" name=\"name\" value=\"" 
-			+ s.getName() + "\" />	<input type=\"hidden\" name=\"birthday\" value=\"" 
-			+ s.getBirthday() + "\" />	<input type=\"hidden\" name=\"mot\" value=\"update\" />"	//добавляется специальный параметр, который определяет действие в этом же сервлете
-			+"<input type = \"submit\" value = \"Редактировать\"/>	</td></form>");
-		pw.println("<form action = \"servStud\" method=\"GET\"><td colspan = \"19\"><input type=\"hidden\" name=\"id\" value=\""
-			+ s.getId() + "\" />	<input type=\"hidden\" name=\"name\" value=\"" 
-			+ s.getName() + "\" />	<input type=\"hidden\" name=\"birthday\" value=\"" 
-			+ s.getBirthday() + "\" />	<input type=\"hidden\" name=\"mot\" value=\"delete\" />"	//добавляется специальный параметр, который определяет действие в этом же сервлете
-			+"<input type = \"submit\" value = \"Удалить\"/>	</td></form>");       
-                pw.println("</tr>");
-            }
-	    pw.println("<form action = \"servStud\" method=\"GET\"><td colspan = \"19\">"
-			+"<input type=\"hidden\" name=\"mot\" value=\"add\" />"				//добавляется специальный параметр, который определяет действие в этом же сервлете
-			+"<input type = \"submit\" value = \"Добавить нового студента\"/>	</td></form>");	    		
-	  		
-        } catch (Exception e) {
-            throw new ServletException("ERROR. you made a mistake when filling in the ID field!",e);
-        }
-        pw.println("</table>");        
-    }
+    private void mAct(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException {
+		try {
+			String act="";
+			if (req.getParameter("act3")!=null){
+				act=req.getParameter("act3");
+			}
+			int id1=888;
+			if (req.getParameter("id")!=null){
+				id1=Integer.parseInt(req.getParameter("id"));//parse РЅСѓР¶РµРЅ РґР»СЏ РїСЂРµРѕР±СЂР°Р·РѕРІР°РЅРёСЏ string РІ int
+			}
+			String name1="empty8";
+			if (req.getParameter("name")!=null){
+				name1=req.getParameter("name");
+			}
+			Student stud = new Student();
+			stud.setId(id1);
+			stud.setName(name1);
+			switch (act){
+				case "add":
+					StudentDao.getInstance().addStudent(stud);
+					//pw.println("РґРѕР»Р¶РµРЅ Р±С‹Р» РґРѕР±Р°РІС‚СЊСЃСЏ СЃС‚СѓРґРµРЅС‚");
+					break;
+				case "update":
+					StudentDao.getInstance().updateStudent(stud);
+					//pw.println("РґРѕР»Р¶РµРЅ Р±С‹Р» РёР·РјРµРЅРёС‚СЊСЃСЏ СЃС‚СѓРґРµРЅС‚");
+					break;
+				case "delete":
+					StudentDao.getInstance().deleteStudent(stud);
+					//pw.println("РґРѕР»Р¶РµРЅ Р±С‹Р» СѓРґР°Р»РёС‚СЊСЃСЏ СЃС‚СѓРґРµРЅС‚");
+					break;
+				default:
+					break;
+			}
+		} catch (Exception e) {
+			throw new ServletException("EРіРі0R. you make some mistake!",e);
+		}
+	}
 
     private void printParam(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
         PrintWriter out = response.getWriter();
-	try {
-	    response.setContentType("text/html;charset=utf-8"); 
+		try {
+	    	response.setContentType("text/html;charset=utf-8");
 
             // Get the values of all request parameters
             Enumeration en = request.getParameterNames();
@@ -122,19 +136,19 @@ public class StudServlet extends javax.servlet.http.HttpServlet {
             	// Get the name of the request parameter
                 String name = (String)en.nextElement();
                 out.println("|----|"+ name);
- 
+
             	// Get the value of the request parameter
                 String value = request.getParameter(name);
- 
+
             	// If the request parameter can appear more than once in the query string, get all values
-                String[] values = request.getParameterValues(name); 
+                String[] values = request.getParameterValues(name);
                 for (int i=0; i<values.length; i++) {
                     out.println(" ==== " + values[i]);
-                }	
+                }
             }
-	    out.close();
-	} catch (Exception e) {
+	    	out.close();
+		} catch (Exception e) {
             throw new ServletException(e);
-        }                
+        }
     }
 }
